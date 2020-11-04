f99ee7fd52be1fbcb5f8892173abdf90192c7d51
Personal-IliyanDimov <ilianmd@abv.bg>
2020-11-04 09:55:55 +0200

    Initial setup

---
+++ src/main/java/org/imd/jaxrs/sample1/springbootjaxrssample1/SpringBootJaxrsSample1Application.java
@@ -0,0 +1,13 @@
+package org.imd.jaxrs.sample1.springbootjaxrssample1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJaxrsSample1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJaxrsSample1Application.class, args);
+	}
+
+}
